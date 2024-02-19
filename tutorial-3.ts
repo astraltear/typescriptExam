11b4d4cb19af28df9d2cc329e8160c1d412acc1e
yjchoi <yjchoi@strato.co.kr>
2024-02-19 17:17:01 +0900

    add

--- tutorial-3.ts
+++ tutorial-3.ts
@@ -5,7 +5,41 @@ export default  export_message1 ;
 function addNumbers(a:number,b:number) :number {
     return a+b;
 }
-
 export {addNumbers};
 
-export const addStrings = (st1:string, str2:string) :string => st1.concat(str2);
+export const addStrings = (str1:string, str2:string="" ) :string => str1.concat(str2);
+export const addAny = (str1:string, str2:string | number) : string => `${str1}${str2}`;  //str1.concat(str2); 
+export const voidReturn  = (str1 :string, str2 :string) :void => console.log(str1, str2);
+
+function introduce(greet :string, ...names :string[]) :string {
+    return `${greet} ${names.join(",")}`;
+}
+export {introduce};
+
+interface User {
+    first:string,
+    last:string,
+}
+
+/*
+?가 붙으면 먼저 ?가 붙은 타입을 조사합니다.
+만약 타입이 define 되었다면 점(.) 
+뒤에 있는 값을 참조하고 만약 타입이 define 되지 않았다면 점(.) 뒤의 값을 참조하지 않습니다.
+
+?? : 널 병합 연산자 왼쪽 피연산자가 null 또는 undefined일 때 오른쪽 피연산자를 반환하고 그렇지 않으면 왼쪽 피연산자를 반환 
+*/
+function getName( user :User ) :string {
+    return `firstName :${user?.first ?? "default first"} lastName : ${user?.last ?? "default last"} `;
+}
+
+export {getName};
+/*
+ts 라면 컴파일 타임에 오류가 발생하므로 아래와 같은 코드를 넣을 수 없다.
+console.log(t3.getName({ first: "astral" }));
+console.log(t3.getName(   ));
+
+그렇지만 js파일에서라면 아래와 같은 코드를 넣을 수 있다.
+이럴때  null에 안전하게 위와 같이 ts파일에서 코드를 작성하면 null 에러를 회피할 수 있다.
+console.log(t3.getName({ first: "astral" }));
+console.log(t3.getName(   ));
+*/
--- tutorial-4.ts
+++ tutorial-4.ts
@@ -5,6 +5,11 @@ console.log(export_message);
 import export_message1 from "./tutorial-3" // export default를 import하는 법 {}  필요 없음 
 console.log(export_message1);
 
-import  * as t3  from "./tutorial-3";
+import  * as t3  from "./tutorial-3"; // 전체 모듈을  import하는 문법 
 console.log(t3.addNumbers(1,2));
 console.log(t3.addStrings("first","second"))
+console.log(t3.addStrings("one param"));
+t3.voidReturn("void","return");
+console.log(t3.introduce("Hello", "Player1", "Player2", "Player3"));
+console.log(t3.getName({first :"astral",last:"tear"} ));
+
