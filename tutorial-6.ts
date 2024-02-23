2e718bc586fd88c844d65b219bd12831d2afa9f2
yjchoi <yjchoi@strato.co.kr>
2024-02-23 09:59:55 +0900

    add

--- tutorial-6.ts
+++ tutorial-6.ts
@@ -18,14 +18,32 @@ interface User2{
     id :string,
     info?:{
         email?:string,
-        memo :string
+        memo? :string
     }
 }
 
 let user1 :User2={id:"tom"};
 console.log(user1);
 
-user1 ={id:"tomas",
+user1 ={
+        id:"tomas",
         info:{  email: "test@test.com" , memo:"memo"  }
-        }
+       }
 console.log(user1);        
+
+function getMemo(user :User2) :string {
+    return user?.info?.memo ?? "NO Memo";
+}
+console.log( getMemo( {id:"red", info :{}} ) );
+
+
+function addWithCallback( x :number, y :number, callback? : (param :string)=> void, paramvalue? :string ) :void {
+    console.log([x,y]);
+    callback?.(paramvalue ?? "");
+}
+
+addWithCallback(123,456,cb1,"parameter1");
+
+function cb1(p1 :string){
+    console.log("cb1::::>",p1);
+}
