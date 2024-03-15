796aaf3f6a58938f2491843c01b857391ce28e02
yjchoi <yjchoi@strato.co.kr>
2024-03-15 09:48:38 +0900

    add

---
+++ tutorial-16-1.ts
@@ -0,0 +1,9 @@
+import hw from "./tutorial-16";
+import {pi,squareTwo} from "./tutorial-16";
+import * as at from "./tutorial-16";
+
+hw();
+console.log("pi:",pi);
+console.log("squareTwo:",squareTwo);
+console.log("at.phi:",at.phi);
+
---
+++ tutorial-16.ts
@@ -0,0 +1,8 @@
+export default function hw(){
+  console.log("hw~");
+}
+
+export var pi = 3.14;
+export let squareTwo = 1.41;
+export const phi = 1.61;
+
